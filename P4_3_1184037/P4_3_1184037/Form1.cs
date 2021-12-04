dc917705c1d7efc59c1707989c594948c5039cdb
VickySafiraKusumaWardani <57054610+VickySafiraKusumaWardani@users.noreply.github.com>
2021-12-04 15:46:02 +0700

    Add files via upload

---
+++ P4_3_1184037/P4_3_1184037/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P4_3_saya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void texthuruf_Leave(object sender, EventArgs e)
+        {
+            if (texthuruf.Text == "")
+            {
+                epWarning.SetError(texthuruf, "Text Tidak Boleh Kosong!");
+                epWrong.SetError(texthuruf, "");
+                epCorrect.SetError(texthuruf, ""); 
+            }
+            else
+            {
+                if ((texthuruf.Text).All(char.IsLetter))
+                {
+                    epWarning.SetError(texthuruf, "");
+                    epWrong.SetError(texthuruf, "");
+                    epCorrect.SetError(texthuruf, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(texthuruf, "Inputan hanya boleh huruf");
+                    epWarning.SetError(texthuruf, "");
+                    epCorrect.SetError(texthuruf, "");
+                }
+            }
+        }
+
+        private void textangka_Leave(object sender, EventArgs e)
+        {
+            if ((textangka.Text).All(char.IsNumber))
+            {
+                epCorrect.SetError(texthuruf, "Betul!");
+                epWarning.SetError(textangka, "");
+                epWrong.SetError(textangka, "");
+            }
+            else
+            {
+                epCorrect.SetError(textangka, "");
+                epWarning.SetError(textangka, "");
+                epWrong.SetError(textangka, "Inputan hanya boleh angka");
+            }
+        }
+
+        private void textemail_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textemail.Text, @"^^[@^\s]+@[@^\s]+(\.[@^\s]+)+$"))
+            {
+                epWarning.SetError(textemail, "");
+                epWrong.SetError(textemail, "");
+                epCorrect.SetError(textemail, "Betul!");
+            }
+            else
+            {
+                epWrong.SetError(textemail, "Format email salah!\ nContoh: a@b.c");
+                epWarning.SetError(textemail, "");
+                epCorrect.SetError(textemail, "");
+            }
+        }
+    }
